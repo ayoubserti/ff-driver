d12b0073a0a8a9636bc422f7c1a2dfa0efdf2981
Ayoub serti <ayb.serti@gmail.com>
2017-09-15 23:04:16 +0000

    new cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,56 @@
 #include "FirefoxDriver.h"
+#include "args.hxx"
+#include <iostream>
 
 int main(int argc, char** argv)
 {
-    FireFoxDriver ffDriver;
-	auto tabList = ffDriver.GetTabList();
-	//ffDriver.NavigateTo(tabList[0], "http://facebook.com");
+
+	//parse command line args
+	args::ArgumentParser parser("FireFox Driver CLI");
+	args::HelpFlag help(parser, "help", "Display this help menu", { 'h', "help" });
+	args::Group flagGroup(parser);
+	args::Flag listTab(flagGroup, "listTabs", "List all opened tab within Firefox", { 'l',"listTabs" });
+	args::ValueFlag<string> newTab(flagGroup, "newTab", "Open new tab within Firefox", { 'n',"new" });
+
+	try
+	{
+		parser.ParseCLI(argc, argv);
+	}
+	catch (args::Help)
+	{
+		std::cout << parser;
+		return 0;
+	}
+	catch (args::ParseError e)
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << parser;
+		return 1;
+	}
+	catch (...)
+	{
+		; //TBD
+	}
 	
-	ffDriver.OpenNewTab();
-	tabList = ffDriver.GetTabList();
-	//ffDriver.CloseTab(tabList[1]);
+	
+	FireFoxDriver ffDriver;
+	if (listTab)
+	{
+		auto tabList = ffDriver.GetTabList();
+		int i=1;
+		for (auto& it : tabList)
+		{
+			
+			cout << "Tab #" << i << ": " << endl;
+			cout << "	" << "title: " << it.GetTitle() << endl;
+			cout << "	" << "URL: " << it.GetURL() << endl;
+			++i;
+		}
+	}
+	if (newTab)
+	{
+		ffDriver.OpenNewTab();
+	}
+	
     return 0;
 }

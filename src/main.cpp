448867b1621803fea439ee92210aa7b6bcffedf1
Ayoub serti <ayb.serti@gmail.com>
2017-09-16 14:56:20 +0000

    handle --newTab and --closeTab arguments

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ int main(int argc, char** argv)
 	args::Group flagGroup(parser);
 	args::Flag listTab(flagGroup, "listTabs", "List all opened tab within Firefox", { 'l',"listTabs" });
 	args::ValueFlag<string> newTab(flagGroup, "newTab", "Open new tab within Firefox", { 'n',"new" });
+	args::ValueFlag<int> closeTab(flagGroup, "closeTab", "Close Tab id", { 'c',"close" });
+
 
 	try
 	{
@@ -44,12 +46,21 @@ int main(int argc, char** argv)
 			cout << "Tab #" << i << ": " << endl;
 			cout << "	" << "title: " << it.GetTitle() << endl;
 			cout << "	" << "URL: " << it.GetURL() << endl;
+			cout << "	" << "actor" << it.GetActor() << endl;
 			++i;
 		}
 	}
 	if (newTab)
 	{
-		ffDriver.OpenNewTab();
+		ffDriver.OpenNewTab(args::get(newTab));
+	}
+	if (closeTab)
+	{
+		int tabToCloseId = args::get(closeTab);
+		auto allTabs = ffDriver.GetTabList();
+		if (allTabs.size() >= tabToCloseId && tabToCloseId >0) {
+			ffDriver.CloseTab(allTabs[tabToCloseId - 1]);
+		}
 	}
 	
     return 0;
